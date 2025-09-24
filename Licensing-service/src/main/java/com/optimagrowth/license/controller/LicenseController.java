281e43af18c0cb67c08543960d62272d654a4391
Denys Khmara <khmaradenis64@gmail.com>
2025-09-24 20:45:21 +0200

    Add Docker support and actuator configuration for License service and add Spring HATEOAS and simple showcase to it

--- Licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ Licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Locale;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/v1/organizations/{organizationId}/license")
 @RequiredArgsConstructor
@@ -24,11 +27,30 @@ public class LicenseController {
     private final LicenseService licenseService;
 
     @GetMapping("/{licenseId}")
-    public ResponseEntity<License> getLicense(
-            @PathVariable("organizationId") String organizationId,
-            @PathVariable("licenseId") String licenseId
-    ) {
+    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
         License license = licenseService.getLicense(licenseId, organizationId);
+        license.add(
+                linkTo(
+                        methodOn(LicenseController.class)
+                                .getLicense(organizationId, licenseId)
+                )
+                        .withSelfRel(),
+                linkTo(
+                        methodOn(LicenseController.class)
+                                .createLicense(organizationId, license, null)
+                )
+                        .withRel("createLicense"),
+                linkTo(
+                        methodOn(LicenseController.class)
+                                .updateLicense(organizationId, license, null)
+                )
+                        .withRel("updateLicense"),
+                linkTo(
+                        methodOn(LicenseController.class)
+                                .deleteLicense(organizationId, licenseId)
+                )
+                        .withRel("deleteLicense")
+                );
         return ResponseEntity.ok(license);
     }
 
@@ -43,8 +65,7 @@ public class LicenseController {
     }
 
     @DeleteMapping("/{licenseId}")
-    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId,
-                                                @PathVariable("licenseId") String licenseId) {
+    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
         return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
     }
 }
